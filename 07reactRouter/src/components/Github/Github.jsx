e34e97aa303c3cb25b6c7423a32799d4bb9d9245
Ayan-004 <ayyanjsk@gmail.com>
2024-12-30 16:52:42 +0530

    react-router

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+function Github() {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('https://api.github.com/users/Ayan-004')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setData(data)
+        })
+    }, [])
+    return (
+        <div className='bg-gray-700 text-white p-4 text-3xl'>Github Followers: {data.followers}
+        <img src={data.avatar_url} alt="Githib Picture" width={300} />
+        </div>
+    )
+}
+
+export default Github
