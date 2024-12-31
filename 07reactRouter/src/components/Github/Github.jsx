828e691c9e97cab82433c7487f691872bd5b3811
Ayan-004 <ayyanjsk@gmail.com>
2024-12-31 15:59:19 +0530

    React router complete

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/Ayan-004')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            setData(data)
-        })
-    }, [])
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/Ayan-004')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // }, [])
     return (
         <div className='bg-gray-700 text-white p-4 text-3xl'>Github Followers: {data.followers}
         <img src={data.avatar_url} alt="Githib Picture" width={300} />
@@ -18,3 +20,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfo = async () => {
+    const response = await fetch("https://api.github.com/users/Ayan-004")
+    return response.json()
+}
