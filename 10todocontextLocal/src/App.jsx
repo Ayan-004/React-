d2ac70a88ad87ed48cfb77450dafbd0c4f9bb81a
Ayan-004 <ayyanjsk@gmail.com>
2025-01-09 22:15:28 +0530

    todo

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,9 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts'
 
 function App() {
   const [todos, setTodos] = useState([])
+
+  const addtodo = (todo) => {
+    setTodos((prev) => [{id: Date.now, ...todo}, ...prev])
+  }
+
+  const updatetodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deletetodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
 
   return (
     <TodoProvider value={{todos, addtodo, updatetodo, deletetodo, toggleComplete}}>
